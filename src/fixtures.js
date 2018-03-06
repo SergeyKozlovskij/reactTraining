9374ffcd1cab0afcac027c6c0bd6e270998267a6
Sergey Kozlovskij <s.kozlovskij@nixsolutions.com>
2018-03-06 12:55:29 +0200

    react screencast, lesson 4

---
+++ src/fixtures.js
@@ -0,0 +1,15 @@
+export default [
+    {
+        "id": "1",
+        "date": "2018-03-06",
+        "title": "Title here",
+        "text": "There is a some text",
+        "comments": [
+            {
+                "id": "1",
+                "user": "user1",
+                "text": "comment text"
+            }
+        ]
+    }
+]
