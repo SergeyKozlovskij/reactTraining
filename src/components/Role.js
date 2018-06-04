3c75823f3de24f86110309ffdb3534602a3451d5
Sergey Kozlovskij <s.kozlovskij@nixsolutions.com>
2018-06-04 13:17:26 +0300

    added show/hide to permission column

--- src/components/Role.js
+++ src/components/Role.js
@@ -2,12 +2,26 @@ import React, {Component} from 'react'
 
 
 class Role extends Component {
+    state = {
+        permissionOpen: false
+    };
+    expandPermission = () => {
+        this.setState({permissionOpen: !this.state.permissionOpen})
+    };
+
     render() {
         const {role, index} = this.props;
         return (
-            <div className="card">
-                <li className="role_li" key={role.id}>{index + 1} {role.roleCode}</li>
-            </div>
+            <tr>
+                <td>{index + 1}</td>
+                <td className="role_li" key={role.id}>{role.roleCode}</td>
+                <td>
+                    <button onClick={this.expandPermission}>>></button>
+                </td>
+                <td>
+                    {this.state.permissionOpen && role.permissions.map(permission => permission.permissionCode).join(", ")}
+                </td>
+            </tr>
         )
     }
 }
