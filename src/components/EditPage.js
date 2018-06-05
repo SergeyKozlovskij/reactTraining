4320708b72fc7a45a6ea28fd6b043da78200c264
Sergey Kozlovskij <s.kozlovskij@nixsolutions.com>
2018-06-05 14:00:26 +0300

    adde available permission list

--- src/components/EditPage.js
+++ src/components/EditPage.js
@@ -1,9 +1,22 @@
 import React, {Component} from 'react'
+import roles from '../fixtures'
+import permissions from '../permissions'
+import _ from 'lodash'
 
 class EditPage extends Component {
     render() {
         let id = window.location.search.substr(1);
-        return (<div>Hello {id}</div>)
+        let role = _.find(roles, {'id': id});
+        let rolePermissions = role.permissions.map(permission => <li key={permission.permissionCode}>{permission.permissionCode}</li>);
+        let availablePermission = _.xorBy(permissions, role.permissions, 'permissionCode').map(permission => <li key={permission.permissionCode}>{permission.permissionCode}</li>);
+        return (<div className="row">
+            <div className="col-md-6"><h2>{role.roleCode}</h2>
+                <ol>{rolePermissions}</ol>
+            </div>
+            <div className="col-md-6"><h2>Available Permissions</h2>
+                <ol>{availablePermission}</ol>
+            </div>
+        </div>)
     }
 }
 
