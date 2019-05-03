2a7382160c3ccf0f17f45bbfce7e2ef82928cb57
kozlovskij <serkozlovskij@yandex.ru>
2019-05-03 19:51:58 +0300

    clean up for new task

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,11 @@
 import React, {Component} from 'react'
 
 import 'bootstrap/dist/css/bootstrap.css'
-import RoleList from './RoleList';
-import EditPage from './EditPage'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            route: window.location.pathname.substr(1)
-        }
-    }
-
-    componentDidMount(){
-        this.setState = {
-            route: window.location.pathname.substr(1)
-        }
-    }
 
     render() {
-        let Child;
-        switch (this.state.route) {
-            case 'edit': Child = EditPage; break;
-            default: Child = RoleList;
-        }
-
-        return (
-            <div className="container">
-                <Child/>
-            </div>
-        )
+        return (<h1>Hello world</h1>)
     }
 }
 
